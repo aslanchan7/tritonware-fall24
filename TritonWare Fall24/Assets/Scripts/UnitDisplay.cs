fa632fc42e11345bf2fd895133d53f021a69bec9
Himir Desai <himirdesai72@gmail.com>
2024-10-10 00:40:37 -0700

    Added Healthbars for all units

--- TritonWare Fall24/Assets/Scripts/UnitDisplay.cs
+++ TritonWare Fall24/Assets/Scripts/UnitDisplay.cs
@@ -6,11 +6,24 @@ using UnityEngine;
 
 public class UnitDisplay : MonoBehaviour
 {
-    public TMP_Text healthText; // temporary text display for testing
     public Unit Unit;
+    public RectTransform HealthForeground;
+    public SpriteRenderer HealthSprite;
+    public Color[] colors = new Color[5];
     public void UpdateDisplay()
     {
-        healthText.text = Unit.Health.ToString();
+        int Health = Unit.Health >= 0 ? Unit.Health : 0;
+        int color = (4 - Health / 20) > 4 ? 4 : (4 - Health / 20);
+        print(Health + " " + color + " " + colors[color]);
+
+
+        HealthSprite.color = colors[color];
+        if (Unit.Health == 100) {
+            gameObject.SetActive(false);
+        } else {
+            gameObject.SetActive(true);
+            HealthForeground.localScale = new Vector3(Health / 100f, 1f, 1f);
+        }
     }
     
 }
