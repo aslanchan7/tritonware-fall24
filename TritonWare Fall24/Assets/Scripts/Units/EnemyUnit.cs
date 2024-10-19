829bc7f92845b88b0285853a94ca7223cf85ecee
perilune42 <jackymei.abc@gmail.com>
2024-10-19 16:56:41 -0700

    units no longer shoot while doing task

--- TritonWare Fall24/Assets/Scripts/Units/EnemyUnit.cs
+++ TritonWare Fall24/Assets/Scripts/Units/EnemyUnit.cs
@@ -162,7 +162,7 @@ public abstract class EnemyUnit : Unit
         {
             float rand = Random.Range(0f, 1f);
             // 10% chance of infecting an allied unit every attack
-            if (rand < infectionAttackChance)
+            if (!(unit is Doctor) && rand < infectionAttackChance)
             {
                 unit.GetInfected();
             }
