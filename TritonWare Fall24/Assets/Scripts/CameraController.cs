b49e6e9e43a1d196216d5bc77216ee5ac3ea7d03
Himir Desai <76738299+Himir-Desai@users.noreply.github.com>
2024-10-20 19:33:39 -0700

    Added zoom mechanics

--- TritonWare Fall24/Assets/Scripts/CameraController.cs
+++ TritonWare Fall24/Assets/Scripts/CameraController.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
@@ -10,12 +12,17 @@ public class CameraController : MonoBehaviour
     public float edgeThreshold = 10f; // Distance from the screen edge to trigger panning
     public Vector2 minCameraPos;     // Minimum camera position (limits)
     public Vector2 maxCameraPos;     // Maximum camera position (limits)
-
+    public float zoomSensitivity = 1f;
     public bool mouseControlsEnabled = true;
+
+    private static Vector2 cameraTargetLocation;
+    private static float zoomTargetOrthographicSize;
 
     private void Awake()
     {
         transform.position = new Vector3(MapManager.Instance.MapSize.x / 2f, MapManager.Instance.MapSize.y / 2f, transform.position.z);
+        zoomTargetOrthographicSize = mainCamera.orthographicSize;   
+        cameraTargetLocation = mainCamera.transform.position;
     }
     void Update()
     {
@@ -27,6 +34,10 @@ public class CameraController : MonoBehaviour
         // Get the width and height of the screen
         float screenWidth = Screen.width;
         float screenHeight = Screen.height;
+
+        float screenAspect = screenWidth / screenHeight;
+        float cameraHeight = mainCamera.orthographicSize * 2;
+        float cameraWidth = cameraHeight * screenAspect;
 
         // KEYBOARD CONTROLS
 
@@ -69,21 +80,36 @@ public class CameraController : MonoBehaviour
             {
                 camMoveVector = new Vector2(mouseHorizontal, -1);
             }
-        }
+            
+            cameraTargetLocation += camMoveVector.normalized * Time.deltaTime * panSpeed;
 
+            // Camera zoom
+            Vector2 mouseScroll = Input.mouseScrollDelta;
+
+            if(mouseScroll.magnitude > 0 ) {
+                float newOrthographicSize = zoomTargetOrthographicSize - mouseScroll.y * zoomSensitivity;
+                zoomTargetOrthographicSize = Math.Clamp(newOrthographicSize, 5, 25);
+
+                float zoomMultiplier = (1 - zoomTargetOrthographicSize / mainCamera.orthographicSize);
+
+                cameraTargetLocation += new Vector2((mouseHorizontal*cameraWidth/2f)*(zoomMultiplier), (mouseVertical*cameraHeight/2f)*(zoomMultiplier));
+            
+                mainCamera.orthographicSize = zoomTargetOrthographicSize;
+            }            
+        }
 
 
         cameraPos += (Vector3)(camMoveVector.normalized * panSpeed * Time.deltaTime);
 
         // Clamp the camera's position to stay within the specified bounds
-
-        float screenAspect = screenWidth / screenHeight;
-        float cameraHeight = mainCamera.orthographicSize * 2;
-        float cameraWidth = cameraHeight * screenAspect;
+        cameraHeight = mainCamera.orthographicSize * 2;
+        cameraWidth = cameraHeight * screenAspect;
         Vector2 newMinCameraPos = new(minCameraPos.x + cameraWidth / 2f, minCameraPos.y + cameraHeight / 2f);
         Vector2 newMaxCameraPos = new(maxCameraPos.x - cameraWidth / 2f, maxCameraPos.y - cameraHeight / 2f);
-        cameraPos.x = Mathf.Clamp(cameraPos.x, newMinCameraPos.x, newMaxCameraPos.x);
-        cameraPos.y = Mathf.Clamp(cameraPos.y, newMinCameraPos.y, newMaxCameraPos.y);
+        // cameraPos.x = Mathf.Clamp(cameraPos.x, newMinCameraPos.x, newMaxCameraPos.x);
+        // cameraPos.y = Mathf.Clamp(cameraPos.y, newMinCameraPos.y, newMaxCameraPos.y);
+        cameraPos.x = Mathf.Clamp(cameraTargetLocation.x, newMinCameraPos.x, newMaxCameraPos.x);
+        cameraPos.y = Mathf.Clamp(cameraTargetLocation.y, newMinCameraPos.y, newMaxCameraPos.y);
 
         // Apply the new camera position
         mainCamera.transform.position = cameraPos;
